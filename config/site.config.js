1e20627013d5a9caf69db129a73a591de9723bc6
赵苦瓜 <62331496+Zhaokugua@users.noreply.github.com>
2022-10-17 20:54:47 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -64,7 +64,7 @@ module.exports = {
       link: 'http://wpa.qq.com/msgrd?v=3&uin=1066168689&site=qq&menu=yes',
     },
     {
-      name: '博客',
+      name: 'Blog',
       link: 'https://blog.jixiaob.cn/',
     },
   ],
